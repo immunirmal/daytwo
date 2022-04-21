9a61e0a975e0be694adeeb968c0e96b99e05ff6e
immunirmal <nirmalimmanuvel@gmail.com>
2022-04-21 17:36:37 +0530

    project added

---
+++ eclipse-workspace/org.java/src/org/java/Employee.java
@@ -0,0 +1,16 @@
+package org.java;
+
+public class Employee {
+	
+private void empId() {
+	System.out.println("675");
+}
+	public static void main(String[]args) {
+		Employee e=new Employee();
+		e.empId();
+		
+		
+		
+	
+}
+}
